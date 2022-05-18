a24824b28779f194ab0548226eaf52c0d3d5a613
psycho-baller <Ramim66809@gmail.com>
2022-05-18 03:15:29 -0600

    small fixes

--- pages/boom-bam-bap.tsx
+++ pages/boom-bam-bap.tsx
@@ -122,10 +122,10 @@ const BoomBamBap = (props: IndexProps) => {
   useEffect(() => {
     // runs when the page runs
     // setPosition({ x: x, y: y });
-    // var randomNum = Math.floor(Math.random() * Object.keys(jokes).length);
-    // while (usedIDs.includes(randomNum)) {
-    //   randomNum = Math.floor(Math.random() * Object.keys(jokes).length);
-    // }
+    var randomNum = Math.floor(Math.random() * Object.keys(jokes).length);
+    while (usedIDs.includes(randomNum)) {
+      randomNum = Math.floor(Math.random() * Object.keys(jokes).length);
+    }
 
     // we use this if statement to make the usedIDs act as a set,
     // but since we added lines 117-119,
@@ -133,9 +133,9 @@ const BoomBamBap = (props: IndexProps) => {
     // since the list resets when it becomes full
     // and we are sure that the randomNum is not in the list thanks to the while loop
     // if (!usedIDs.includes(randomNum)) {
-    // setUsedIDs([...usedIDs, randomNum]);
+    setUsedIDs([...usedIDs, randomNum]);
     // }
-    // setID(randomNum);
+    setID(randomNum);
 
     boomBapPow();
   }, []);
